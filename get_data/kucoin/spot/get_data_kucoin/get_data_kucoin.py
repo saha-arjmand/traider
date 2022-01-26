7f4d3114d4e935d611417549677f85c49dfee0f2
saha-arjmand <s.amirhossein.arjmand@gmail.com>
2022-01-26 22:08:07 +0330

    Create Seconds to utc time function in time.py

--- get_data/kucoin/spot/get_data_kucoin/get_data_kucoin.py
+++ get_data/kucoin/spot/get_data_kucoin/get_data_kucoin.py
@@ -1,4 +1,4 @@
-import traider.utils.time.time as timeutc
+from traider.utils.time.time import Calculate_time
 from traider.get_data.kucoin.spot.url import CreateUrl
 import requests
 
@@ -8,8 +8,8 @@ import requests
 class OneMinuteSpotData():
 
     def single_One_Minute_Last_Data(self):
-        first_time = timeutc.firstTime
-        second_time = timeutc.lastTime
+        first_time = Calculate_time.firstTime
+        second_time = Calculate_time.lastTime
         url = CreateUrl.URL(second_time, first_time, "BTC-USDT", "1min")
         response = requests.get(url=url).json()
         try:
@@ -18,11 +18,11 @@ class OneMinuteSpotData():
         except:
             while(response['code'] != 200000):
                 response = requests.get(url=url).json()
-            return response['data'][0]
+                return response['data'][0]
 
     def multi_One_Minute_Data(self,number_of_Candles):
-        first_time = timeutc.firstTime - (number_of_Candles - 1) * 60
-        second_time = timeutc.lastTime
+        first_time = Calculate_time.firstTime - (number_of_Candles - 1) * 60
+        second_time = Calculate_time.lastTime
         url = CreateUrl.URL(second_time, first_time, "BTC-USDT", "1min")
         response = requests.get(url=url).json()
         try:
@@ -31,10 +31,10 @@ class OneMinuteSpotData():
         except:
             while(response['code'] != 200000):
                 response = requests.get(url=url).json()
-            return response['data']
+                return response['data']
 
 
 
-candle = OneMinuteSpotData()
-for anyItem in candle.multi_One_Minute_Data(5):
-    print(anyItem)
+# candle = OneMinuteSpotData()
+# for anyItem in candle.multi_One_Minute_Data(3):
+#     print(anyItem)
--- utils/time/time.py
+++ utils/time/time.py
@@ -1,10 +1,15 @@
 import datetime
 
-# now utc time
-current_utc_time = datetime.datetime.utcnow()
-current_uts_time_to_seconds = int((current_utc_time - datetime.datetime(1970,1,1)).total_seconds())
+class Calculate_time:
+    # now utc time
+    current_utc_time = datetime.datetime.utcnow()
+    current_uts_time_to_seconds = int((current_utc_time - datetime.datetime(1970, 1, 1)).total_seconds())
 
-# minute timframe
-# time must be from 1 min ago to 2 minutes past
-lastTime  = current_uts_time_to_seconds - current_utc_time.second
-firstTime = lastTime - 60
+    # minute timframe
+    # time must be from 1 min ago to 2 minutes past
+    lastTime = current_uts_time_to_seconds - current_utc_time.second
+    firstTime = lastTime - 60
+
+    def convert_second_to_utc_time(self,seconds):
+        std_time = datetime.datetime.utcfromtimestamp(seconds)
+        return std_time

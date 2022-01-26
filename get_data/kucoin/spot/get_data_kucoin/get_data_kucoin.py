5b040578a05159a087faac43f05caaf03a3d8b0f
saha-arjmand <s.amirhossein.arjmand@gmail.com>
2022-01-26 19:22:42 +0330

    add MultiOneMinuteData Candle Class

--- get_data/kucoin/spot/get_data_kucoin/get_data_kucoin.py
+++ get_data/kucoin/spot/get_data_kucoin/get_data_kucoin.py
@@ -6,19 +6,35 @@ import requests
 
 # Get Data From API
 class OneMinuteSpotData():
-    def singleOneMinuteData(self):
+
+    def single_One_Minute_Last_Data(self):
         first_time = timeutc.firstTime
         second_time = timeutc.lastTime
         url = CreateUrl.URL(second_time, first_time, "BTC-USDT", "1min")
+        response = requests.get(url=url).json()
         try:
-            response = requests.get(url=url).json()
+            response['code'] == 200000
             return response['data'][0]
         except:
             while(response['code'] != 200000):
                 response = requests.get(url=url).json()
             return response['data'][0]
 
+    def multi_One_Minute_Data(self,number_of_Candles):
+        first_time = timeutc.firstTime - (number_of_Candles - 1) * 60
+        second_time = timeutc.lastTime
+        url = CreateUrl.URL(second_time, first_time, "BTC-USDT", "1min")
+        response = requests.get(url=url).json()
+        try:
+            response['code'] == 200000
+            return response['data']
+        except:
+            while(response['code'] != 200000):
+                response = requests.get(url=url).json()
+            return response['data']
+
 
 
 candle = OneMinuteSpotData()
-print(candle.singleOneMinuteData())
+for anyItem in candle.multi_One_Minute_Data(5):
+    print(anyItem)

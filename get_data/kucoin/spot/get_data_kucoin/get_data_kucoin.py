2270d46739d077b18e5010017d9efdbd390c5323
saha-arjmand <s.amirhossein.arjmand@gmail.com>
2022-02-03 00:38:28 +0330

    seperated date from time in first column and create that func

--- get_data/kucoin/spot/get_data_kucoin/get_data_kucoin.py
+++ get_data/kucoin/spot/get_data_kucoin/get_data_kucoin.py
@@ -19,7 +19,7 @@ class OneMinuteSpotData:
         self.number_of_candles = number_of_candles
 
     '''done'''
-    def __get_data(self):
+    def get_data(self):
         # this parameter send from constructor init method
         number_of_candles = self.number_of_candles
         first_time = Calculate_time.firstTime - (number_of_candles - 1) * 60
@@ -40,7 +40,7 @@ class OneMinuteSpotData:
     def past_data_stdTime(self):
 
         # Convert list obj to numpy array 2D obj
-        data = np.array(self.__get_data())
+        data = np.array(self.get_data())
 
         # data.shape[0] this command give us number of rows of our matrix
         i = 0
@@ -48,17 +48,49 @@ class OneMinuteSpotData:
         calcobj = Calculate_time()
         while i < data.shape[0]:
             # data[i,0] give us the index0 (time) of our matrix
-            data[i, 0] = calcobj.convert_second_to_utc_time(int(data[i, 0]))
+            data[i, 0] = convertData = calcobj.convert_second_to_utc_time(int(data[i, 0]))
             i += 1
 
         return data
 
     '''done'''
+    def split_date_time(self):
+
+        # get data from past function : stdTime
+        data = np.array(self.past_data_stdTime())
+
+        finalList = list()
+        i = 0
+
+        # i must iterate the items in the array and change time column
+        for anyItem in data:
+            # 1 : delete first column time
+            dataInfo = np.delete(anyItem, 0)
+            # 2 : get the first column and extract date from it
+            dateInfo = (anyItem[0].split(' '))[0]
+            # 3 : get the first column and extract time from it
+            timeInfo = (anyItem[0].split(' '))[1]
+
+            # 4 : add date that extract in step 2 to final array
+            fullData1 = np.insert(dataInfo, 0, dateInfo, axis=0)
+            # 5 : add time that extract in step 3 to final array
+            # for add to a numpy array the data must be a tuple so we casting data to tuple
+            fullData2 = tuple(np.insert(fullData1, 1, timeInfo, axis=0))
+
+            # 6 : we must add fullData2 to a 2D array
+            finalList.append(fullData2)
+
+            i += 1
+
+        final = np.array(finalList)
+        return final
+
+    '''done'''
     def data_sorting(self):
-        data = self.past_data_stdTime()
+        data = self.split_date_time()
 
         df = pd.DataFrame()
-        df["time"], df["open"], df["close"], \
+        df["date"], df["time"], df["open"], df["close"], \
             df["high"], df["Low"], df["volume"], df["amount"] = data.T
 
         # this code set time column to our index dataframe
@@ -79,7 +111,14 @@ class OneMinuteSpotData:
 
 
 
-s = OneMinuteSpotData(2)
-# print(s.data_sorting())
-s.save_data_db()
+s = OneMinuteSpotData(10)
+data1 = s.data_sorting()
+print(data1)
+
+
+
+
+
+
+
 

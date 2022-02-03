09c4fa8398b0fc31f142027502f746476c43c344
saha-arjmand <s.amirhossein.arjmand@gmail.com>
2022-02-03 11:08:12 +0330

    fix bug from time response and mix 2 func for it

--- get_data/kucoin/spot/get_data_kucoin/get_data_kucoin.py
+++ get_data/kucoin/spot/get_data_kucoin/get_data_kucoin.py
@@ -1,3 +1,5 @@
+import datetime
+
 from traider.utils.time.time import Calculate_time
 from traider.get_data.kucoin.spot.url import CreateUrl
 from traider.database import database
@@ -37,39 +39,27 @@ class OneMinuteSpotData:
                 return response.json()['data']
 
     '''done'''
-    def past_data_stdTime(self):
+    def convert_std_datetime(self):
 
         # Convert list obj to numpy array 2D obj
         data = np.array(self.get_data())
 
-        # data.shape[0] this command give us number of rows of our matrix
-        i = 0
-        # with calcobj.convert_second_to_utc_time we convert seconds to std time
-        calcobj = Calculate_time()
-        while i < data.shape[0]:
-            # data[i,0] give us the index0 (time) of our matrix
-            data[i, 0] = convertData = calcobj.convert_second_to_utc_time(int(data[i, 0]))
-            i += 1
-
-        return data
-
-    '''done'''
-    def split_date_time(self):
-
-        # get data from past function : stdTime
-        data = np.array(self.past_data_stdTime())
-
         finalList = list()
         i = 0
+        calcobj = Calculate_time()
 
         # i must iterate the items in the array and change time column
         for anyItem in data:
             # 1 : delete first column time
             dataInfo = np.delete(anyItem, 0)
-            # 2 : get the first column and extract date from it
-            dateInfo = (anyItem[0].split(' '))[0]
-            # 3 : get the first column and extract time from it
-            timeInfo = (anyItem[0].split(' '))[1]
+
+            # Date
+            # 2 : Gain date from convert first column (seconds) to std uts date
+            dateInfo = calcobj.convert_second_to_utc_time(int(data[i, 0])).date()
+
+            # Time
+            # 2 : Gain time from convert first column (seconds) to std uts time
+            timeInfo = calcobj.convert_second_to_utc_time(int(data[i, 0])).time()
 
             # 4 : add date that extract in step 2 to final array
             fullData1 = np.insert(dataInfo, 0, dateInfo, axis=0)
@@ -83,11 +73,13 @@ class OneMinuteSpotData:
             i += 1
 
         final = np.array(finalList)
+
+
         return final
 
     '''done'''
     def data_sorting(self):
-        data = self.split_date_time()
+        data = self.convert_std_datetime()
 
         df = pd.DataFrame()
         df["date"], df["time"], df["open"], df["close"], \
@@ -99,14 +91,16 @@ class OneMinuteSpotData:
 
     '''save past data to database'''
     def save_data_db(self):
+
         db = database.DataBase()
         if db.isExist_db():
-            print("save")
+
             my_conn = create_engine(
             f"mysql+pymysql://{secrets.dbuser}:{secrets.dbpass}@{secrets.dbhost}/{secrets.dbname}")
             self.data_sorting().to_sql(con=my_conn, name='1mindata', if_exists='append', index=True)
+            print("save data to databases")
         else:
-            print("false")
+            print("database not found !")
 
 
 
@@ -114,9 +108,6 @@ class OneMinuteSpotData:
 s = OneMinuteSpotData(10)
 data1 = s.data_sorting()
 print(data1)
-
-
-
 
 
 
